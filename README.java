eb331f66226fe0fadacea70a4eacd2e8f6cddc2f
mahendiran43 <kmahee000@gmail.com>
2016-07-09 15:53:43 +0530

    finish

---
+++ README.java
@@ -0,0 +1,21 @@
+# positive
+import java.io.*;
+class demo
+{
+public static void main(string[]args)
+{
+int i=4;
+if(i<0)
+{
+System.out.println("num is negative");
+}
+else
+{
+System.out.println("num is positive");
+}
+if(i==0)
+{
+System.out.println("num is zero");
+}
+}
+}
